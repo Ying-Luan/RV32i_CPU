6bc2fe7443213d208ec10048cf5d97e8ddd80b1e
Ying-Luan <3433800035@qq.com>
2025-09-14 14:32:27 +0800

    update: 调整目录结构

---
+++ test/include/timer.h
@@ -0,0 +1,9 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#define TIMER_BASE (0x20000000)
+#define TIMER_CTRL (TIMER_BASE + (0x00))
+#define TIMER_COUNT (TIMER_BASE + (0x04))
+#define TIMER_VALUE (TIMER_BASE + (0x08))
+
+#endif
